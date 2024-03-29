2b7cd752f25265cc9312da2f0ed9911253ffa93f
Fiddar Putri <pfiddar@gmail.com>
2024-03-29 18:09:00 +0700

    Adding description product

--- assets/api-product.js
+++ assets/api-product.js
@@ -5,43 +5,50 @@ let produk = [{
     id : 1,
     nama : "Chessecake",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqi4u0x9sYoyI53hxrTB-q5xR7amd8UrWj3Q&usqp=CAU",
-    harga : "Rp. 20.000"
+    harga : "Rp. 20.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 2, 
     nama : "Choco Muffin",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj66Y47aMrlY3zLZYnOPnK-gFp7q4ucupD-w&usqp=CAU",
-    harga : "Rp. 10.000"
+    harga : "Rp. 10.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 3, 
     nama : "Chiffon Cake",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGtFHiPO61y9cO5KEzI5ksSborCuCEe-7R5Q&usqp=CAU",
-    harga : "Rp. 25.000"
+    harga : "Rp. 25.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 4, 
     nama : "Croissant",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq86INfgcDFfiaMYw8768L-3eEBhpXV0sGxQ&usqp=CAU",
-    harga : "Rp. 20.000"
+    harga : "Rp. 20.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 5, 
     nama : "Strawberry Chessecake",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAG4LfhcfWMXcOeVzq6Yhe-jrg3jy-_Y6Ow&usqp=CAU",
-    harga : "Rp. 45.000"
+    harga : "Rp. 45.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 6, 
     nama : "Nutella Stuffed Cookies",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUAIG8yEoS1BmkZ0crgEsAAvcvapl9aSjf_A&usqp=CAU",
-    harga : "Rp. 15.000"
+    harga : "Rp. 15.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 },
 {
     id : 7, 
     nama : "Biscoff Stuffed Cookies",
     img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnBHEMDvWEtoRoO8Y624aSyK2fygrS9k6iSw&usqp=CAU",
-    harga : "Rp. 15.000"
+    harga : "Rp. 15.000",
+    deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing"
 }];
 let fetchOption = {
     method : "POST",
@@ -63,8 +70,8 @@ fetch(endpoint, fetchOption)
                                 </div>
                                 <div class="card-footer">
                                     <h3 class="card-title"><b>${element.nama}</b></h3><br>
-                                    <p>Teh hijau yang menghangatkan dan menenangkan</p>
-                                    <div class="row">
+                                    <p>${element.deskripsi}</p>
+                                    <div class="row align-items-center">
                                         <div class="col-sm-8">
                                             <h4 class="card-title">${element.harga}</h4>
                                         </div>
